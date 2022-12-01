c15a8566af198232653f4026f0584a4792b555ff
Magnus Johannes Schröder <magnus.schroeder@coyoapp.com>
2022-12-01 21:03:19 +0200

    day 1 improvements

--- 01/solution.py
+++ 01/solution.py
@@ -5,13 +5,13 @@ calorie_sums = list()
 
 calorie_sum = 0
 for line in lines:
-    if line == "":
+    if line == '':
         calorie_sums.append(calorie_sum)
         calorie_sum = 0
     else:
         calorie_sum += int(line)
 
-calorie_sums.sort(reverse=True)
+calorie_sums.reverse()
 
 print(calorie_sums[0])
 print(sum(calorie_sums[0:2]))
