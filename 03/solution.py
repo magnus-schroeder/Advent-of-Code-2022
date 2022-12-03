30e1ad1a6a3552ca631e8125a44e7def1d5f66bb
Magnus Johannes Schröder <magnus.schroeder@coyoapp.com>
2022-12-03 14:12:02 +0200

    day 3 improvements

--- 03/solution.py
+++ 03/solution.py
@@ -15,18 +15,18 @@ first_priority_sum = 0
 second_priority_sum = 0
 for i in range(len(lines)):
     line = lines[i]
-    firstComp = line[0:int(len(line) / 2)]
-    secondComp = line[int(len(line) / 2):]
+    firstComp = line[:len(line) // 2]
+    secondComp = line[len(line) // 2:]
 
-    match = str(list(filter(lambda s: s in secondComp, firstComp)).pop())
-    first_priority_sum += get_priority(match)
+    first_match = str(list(filter(lambda s: s in secondComp, firstComp)).pop())
+    first_priority_sum += get_priority(first_match)
 
     if i % 3 == 0:
         secondElf = lines[i + 1]
         thirdElf = lines[i + 2]
 
-        match = str(list(filter(lambda s: s in secondElf and s in thirdElf, line)).pop())
-        second_priority_sum += get_priority(match)
+        second_match = str(list(filter(lambda s: s in secondElf and s in thirdElf, line)).pop())
+        second_priority_sum += get_priority(second_match)
 
 print(first_priority_sum)
 print(second_priority_sum)
