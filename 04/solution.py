fcbf4f5e46ae59e9d64fab0b757673f3b30f82c0
Magnus Johannes Schröder <magnus.schroeder@coyoapp.com>
2022-12-04 19:29:36 +0200

    day 4 improvements

--- 04/solution.py
+++ 04/solution.py
@@ -1,16 +1,16 @@
 with open('input.txt') as input_file:
     lines = [line.rstrip() for line in input_file]
 
-lines = list(map(lambda x: list(map(lambda y: y.split('-'), x.split(','))), lines))
+lines = list(map(lambda x: list(map(lambda y: [int(i) for i in y.split('-')], x.split(','))), lines))
 
 first_counter = 0
 second_counter = 0
 for line in lines:
-    (first_start, first_end) = line[0]
-    (second_start, second_end) = line[1]
+    first_start, first_end = line[0]
+    second_start, second_end = line[1]
 
-    first_set = set(range(int(first_start), int(first_end) + 1))
-    second_set = set(range(int(second_start), int(second_end) + 1))
+    first_set = set(range(first_start, first_end + 1))
+    second_set = set(range(second_start, second_end + 1))
 
     if first_set.issubset(second_set) or second_set.issubset(first_set):
         first_counter += 1
